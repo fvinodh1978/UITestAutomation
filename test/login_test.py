acb40a447876dbd913cf0cf853963c361c5928db
fvinodh1978 <f.vinodhfranklin@gmail.com>
2024-03-28 12:11:26 -0400

    Added new Testcases and pytest.ini files

--- test/login_test.py
+++ test/login_test.py
@@ -8,12 +8,14 @@ from webdriver_manager.core.driver_cache import DriverCacheManager
 from webdriver_manager.core.os_manager import OperationSystemManager
 from webdriver_manager.firefox import GeckoDriverManager
 
+# this test class covers all the scenarios related to authentication
+class TestLogin:
+    # @pytest.mark.ignore
+    def test_title(self):
+        expected_title="Automation Remarks"
+        driver = webdriver.Firefox()
+        driver.get("http://automation-remarks.com")
+        actual_title=driver.title
 
-def test_title():
-    expected_title="Automation Remarks"
-    driver = webdriver.Firefox()
-    driver.get("http://automation-remarks.com")
-    actual_title=driver.title
-
-    assert driver.title == expected_title
-    driver.quit()
+        assert driver.title == expected_title
+        driver.quit()
--- test/sample_test.py
+++ test/sample_test.py
@@ -1,4 +1,5 @@
 import sys
+import pytest
 
 # adding src to the system path
 # sys.path.insert(0, '/home/circleci/circleci-python/src')
@@ -6,10 +7,20 @@ import sys
 sys.path.insert(0, "C:\\Personal\\Vinodh\\Projects\\PythonProjects\\Workspace\\UITestFramework\\src")
 from sample import inc
 
-def test_answer():
-    print("abc")
-    assert inc(3) == 4
+class TestSample:
+    @pytest.mark.sanity
+    def test_answer(self):
+        print("abc")
+        assert inc(3) == 4
+
+    @pytest.mark.functional
+    def test_product(self):
+        print("abc")
+        assert inc(3) == 4    
+
+    @pytest.mark.regression
+    def test_division(self):
+        print("abc")
+        assert inc(3) == 4 
 
 
-if __name__ =='__main__':
-    test_answer()
